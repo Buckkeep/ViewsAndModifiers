c87fa0d514252a0f58a1751a0c644b2e64a036f0
Neeta Buhecha <neeta.b@gmail.com>
2024-05-02 14:44:49 +0100

    Custom containers

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -7,28 +7,29 @@
 
 import SwiftUI
 
-struct Title: ViewModifier {
-    func body(content: Content) -> some View {
-        content
-            .font(.largeTitle)
-            .foregroundStyle(.white)
-            .padding()
-            .background(.blue)
-            .clipShape(.rect(cornerRadius: 10))
-    }
-}
-
-extension View {
-    func titleStyle() -> some View {
-        modifier(Title())
+struct GridStack<Content: View>: View {
+    let rows: Int
+    let columns: Int
+    let content: (Int, Int) -> Content
+    
+    var body: some View {
+        VStack {
+            ForEach(0..<rows, id: \.self) {row in
+                HStack {
+                    ForEach(0..<columns, id: \.self) {column in
+                    content(row, column)}
+                }
+            }
+        }
     }
 }
 
 struct ContentView: View {
     
     var body: some View {
-        Text("Hello World")
-            .titleStyle()
+        GridStack(rows: 4, columns: 4) {
+            row, col in Text("R\(row) C\(col)")
+        }
     }
 }
 
